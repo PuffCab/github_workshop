42caaa88b7e9239ec2485961b7e1e8c68add7980
PuffCab <berlinkollner@gmail.com>
2023-06-07 15:02:48 +0200

    lots of console.logs

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 console.log("first message");
 console.log("second message");
 console.log("third message");
+console.log("third message");
+console.log("third message");
+console.log("third message");
+console.log("third message");
